d30a035f43a0468386e56a22f177e97f49b1be26
Shakirahadjih <70700845+Shakirahadjih@users.noreply.github.com>
2020-10-27 12:58:31 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,25 @@
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
 
- $('.panel-collapse').on('show.bs.collapse', function () {
-    $(this).siblings('.panel-heading').addClass('active');
-  });
+var navbar = document.querySelector(".navbar")
+var ham = document.querySelector(".ham")
 
-  $('.panel-collapse').on('hide.bs.collapse', function () {
-    $(this).siblings('.panel-heading').removeClass('active');
-  });
+// toggles hamburger menu in and out when clicking on the hamburger
+function toggleHamburger(){
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("showClose")
+}
 
+ham.addEventListener("click", toggleHamburger)
+
+// toggle when clicking on links
+
+// METHOD 1
+var menuLinks = document.querySelectorAll(".menuLink")
+menuLinks.forEach( 
+  function(menuLink) { 
+    menuLink.addEventListener("click", toggleHamburger) 
+  }
+)
 
 
